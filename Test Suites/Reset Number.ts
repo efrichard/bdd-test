768e8d07482f20e8b507c9edacd16c032215b210
Efran Pasaribu <'efranr@xl.co.id'>
2024-06-25 09:56:36 +0700

    Test Suite Collection

---
+++ Test Suites/Reset Number.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Reset Number</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>87d62457-c4f5-4ad8-8be5-79f3bf4681f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>865fcf4d-663c-4d80-842f-b9e3ff59442f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/Verify AC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
